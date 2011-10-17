942af1a2c6d399a23bf77539ad8fe05c0ecf6c90
Kaikz <xkaikz@gmail.com>
2011-10-17 15:09:03 +1300

    attempted permissions fix.

--- src/net/pwncraft/kaikz/instamode/InstaMode.java
+++ src/net/pwncraft/kaikz/instamode/InstaMode.java
@@ -1,7 +1,5 @@
 package net.pwncraft.kaikz.instamode;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
@@ -9,12 +7,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.config.Configuration;
 
-public class InstaMode extends JavaPlugin {
-	public Configuration config;
-	public Map<String, String> worldStatus = new HashMap<String, String>();
-	
+public class InstaMode extends JavaPlugin {	
     @Override
     public void onDisable() {
         PluginDescriptionFile pdfFile = this.getDescription();
@@ -23,26 +17,8 @@ public class InstaMode extends JavaPlugin {
 
     @Override
     public void onEnable() {
-    	config = getConfiguration();
         PluginDescriptionFile pdfFile = this.getDescription();
         System.out.println(pdfFile.getName() + " v" + pdfFile.getVersion() + " is enabled!");
-    }
-    
-    public boolean hasPermission(String mode, Player player) {
-        if (mode.equalsIgnoreCase("survival")) {
-            if (player.hasPermission("*") || player.hasPermission("instamode.*") || player.hasPermission("instamode.survival")) {
-                return true;
-            } else {
-                return false;
-            }
-        } else if (mode.equalsIgnoreCase("creative")) {
-            if (player.hasPermission("*") || player.hasPermission("instamode.*") || player.hasPermission("instamode.creative")) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        return false;
     }
     
     @Override
@@ -57,7 +33,7 @@ public class InstaMode extends JavaPlugin {
                 player.sendMessage(ChatColor.RED + "You didn't give us a game mode!");
                 return true;
             } else if (args.length == 1) {
-            	if ((args[0].equalsIgnoreCase("survival") || args[0].equalsIgnoreCase("s")) && hasPermission("survival", player)) {
+            	if ((args[0].equalsIgnoreCase("survival") || args[0].equalsIgnoreCase("s")) && (player.hasPermission("*") || player.hasPermission("instamode.*") || player.hasPermission("instamode.survival"))) {
             		if (player.getGameMode() == GameMode.SURVIVAL) {
             			player.sendMessage(ChatColor.RED + "You're already in Survival mode!");
             			return true;
@@ -66,7 +42,7 @@ public class InstaMode extends JavaPlugin {
             			player.sendMessage(ChatColor.GREEN + "Set to Survival Mode!");
             			return true;
             		}
-            	} else if ((args[0].equalsIgnoreCase("creative") || args[0].equalsIgnoreCase("c")) && hasPermission("creative", player)) {
+            	} else if ((args[0].equalsIgnoreCase("creative") || args[0].equalsIgnoreCase("c")) && (player.hasPermission("*") || player.hasPermission("instamode.*") || player.hasPermission("instamode.creative"))) {
                     if (player.getGameMode() == GameMode.CREATIVE) {
             		player.sendMessage(ChatColor.RED + "You're already in Creative mode!");
             		return true;
