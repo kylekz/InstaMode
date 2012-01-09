568a74d3e790de4c91b478e8a93bc1eacbe74ebe
Matthieu Blottière <greizgh@gmail.com>
2012-01-09 17:37:47 +0100

    Added ability to change in-game messages through config.yml

--- src/me/freeplaynz/instamode/InstaMode.java
+++ src/me/freeplaynz/instamode/InstaMode.java
@@ -9,14 +9,39 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class InstaMode extends JavaPlugin {	
+	String nomode;
+	String already_creative;
+	String already_survival;
+	String info_creative, info_survival;
     @Override
     public void onDisable() {
+    	this.saveConfig();
         PluginDescriptionFile pdfFile = this.getDescription();
         System.out.println(pdfFile.getName() + " v" + pdfFile.getVersion() + " is disabled!");
     }
 
     @Override
     public void onEnable() {
+    	if (this.getConfig().isSet("error.nomode")==false){
+	    	this.getConfig().set("error.nomode", "You didn't give us a game mode!");
+    	}
+    	if (this.getConfig().isSet("error.creative")==false){
+	    	this.getConfig().set("error.creative", "You're already in Creative mode!");
+    	}
+    	if (this.getConfig().isSet("error.survival")==false){
+	    	this.getConfig().set("error.survival", "You're already in Survival mode!");
+    	}
+    	if (this.getConfig().isSet("info.creative")==false){
+	    	this.getConfig().set("info.creative", "Set to Creative Mode!");
+    	}
+    	if (this.getConfig().isSet("info.survival")==false){
+	    	this.getConfig().set("info.survival", "Set to Survival Mode!");
+    	}
+    	nomode = this.getConfig().getString("error.nomode", "You didn't give us a game mode!");
+    	already_creative = this.getConfig().getString("error.creative", "You're already in Creative mode!");
+    	already_survival = this.getConfig().getString("error.survival", "You're already in Survival mode!");
+    	info_creative = this.getConfig().getString("info.creative", "Set to Creative Mode!");
+    	info_survival = this.getConfig().getString("info.survival", "Set to Survival Mode!");
         PluginDescriptionFile pdfFile = this.getDescription();
         System.out.println(pdfFile.getName() + " v" + pdfFile.getVersion() + " is enabled!");
     }
@@ -30,25 +55,25 @@ public class InstaMode extends JavaPlugin {
         
         if (commandLabel.equals("mode")) {
             if (args.length < 0) {
-                player.sendMessage(ChatColor.RED + "You didn't give us a game mode!");
+                player.sendMessage(ChatColor.RED + nomode);
                 return true;
             } else if (args.length == 1) {
             	if ((args[0].equalsIgnoreCase("survival") || args[0].equalsIgnoreCase("s")) && (player.hasPermission("*") || player.hasPermission("instamode.*") || player.hasPermission("instamode.survival"))) {
                     if (player.getGameMode() == GameMode.SURVIVAL) {
-            		player.sendMessage(ChatColor.RED + "You're already in Survival mode!");
+            		player.sendMessage(ChatColor.RED + already_survival);
             		return true;
                     } else {
             		player.setGameMode(GameMode.SURVIVAL);
-            		player.sendMessage(ChatColor.GREEN + "Set to Survival Mode!");
+            		player.sendMessage(ChatColor.GREEN + info_survival);
             		return true;
                     }
             	} else if ((args[0].equalsIgnoreCase("creative") || args[0].equalsIgnoreCase("c")) && (player.hasPermission("*") || player.hasPermission("instamode.*") || player.hasPermission("instamode.creative"))) {
                     if (player.getGameMode() == GameMode.CREATIVE) {
-            		player.sendMessage(ChatColor.RED + "You're already in Creative mode!");
+            		player.sendMessage(ChatColor.RED + already_creative);
             		return true;
                     } else {
             		player.setGameMode(GameMode.CREATIVE);
-            		player.sendMessage(ChatColor.GREEN + "Set to Creative Mode!");
+            		player.sendMessage(ChatColor.GREEN + info_creative);
             		return true;
                     }
             	}
